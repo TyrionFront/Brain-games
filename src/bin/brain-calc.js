af32ee9eb9632476c4b2067e194e081d1ded0dc6
TyrionFront <GingerTormund@protonmail.com>
2018-07-12 22:52:47 +0300

    chore(bin-files) redact source of import

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import simpleCalcGame from '../games/simpleCalcGame';
+import simpleCalcGame from '../games/simpleCalc';
 
 simpleCalcGame();
--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import isEvenGame from '../games/isEvenGame';
+import isEvenGame from '../games/isEven';
 
 isEvenGame();
