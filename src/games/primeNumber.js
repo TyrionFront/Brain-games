c4f7565f5a605257027939ae43d1c3c9093e9a95
TyrionFront <GingerTormund@protonmail.com>
2018-07-15 17:58:42 +0300

    refactor: improve calculations

--- src/games/primeNumber.js
+++ src/games/primeNumber.js
@@ -2,11 +2,11 @@ import makeGame from '../gameEngine';
 import getRandomNum from '../utils';
 
 const description = 'Is this number prime?';
-const startDivider = 2;
 
 const isPrime = (number) => {
+  const startDivider = 2;
   const iter = (divider) => {
-    if (number === divider) { return true; }
+    if (Math.abs(number) === divider || Math.abs(number) === 1) { return true; }
     if (number % divider === 0) { return false; }
     const newDivider = divider + 1;
     return iter(newDivider);
